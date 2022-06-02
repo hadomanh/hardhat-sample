7f7d05adf3aefe855a1c778d0af59d6c3a91a599
hadomanh <hadomanh99@gmail.com>
2022-06-02 11:50:51 +0700

    Update

--- deploy/1_deploy_contract.ts
+++ deploy/1_deploy_contract.ts
@@ -1,12 +1,11 @@
-module.exports = async ({ ethers, deployments, hardhatArguments } : any) => {
-    // const network = hardhatArguments.network ? hardhatArguments.network : 'development';
-    const { deploy } = deployments;
-    const [ owner ] = await ethers.getSigners();
+module.exports = async ({ ethers, deployments, hardhatArguments }: any) => {
+  // const network = hardhatArguments.network ? hardhatArguments.network : 'development';
+  const { deploy } = deployments;
+  const [owner] = await ethers.getSigners();
 
-    await deploy('WBNB', {
-        from: owner.address,
-        log: true,
-        args: []
-    });
-
+  await deploy("WBNB", {
+    from: owner.address,
+    log: true,
+    args: [],
+  });
 };
