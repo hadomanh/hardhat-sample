0d21fc65d15293ca6a3e986de8631e7de4460795
hadomanh <hadomanh99@gmail.com>
2022-06-08 17:31:34 +0700

    Update

--- contracts/WBNB.sol
+++ contracts/WBNB.sol
@@ -1,11 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract WBNB is ERC20, Ownable {
-    constructor () ERC20("Wrapped BNB", "WBNB")
-    {
-        _mint(msg.sender, 10000 * 10 ** decimals());
+contract WBNB is
+    ERC20("Wrapped BNB", "WBNB"),
+    ERC20Burnable,
+    ERC20Capped,
+    AccessControl {
+
+    constructor (uint256 _cap) ERC20Capped(_cap) {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function _mint(address _to, uint256 _amount) internal override(ERC20, ERC20Capped) {
+        super._mint(_to, _amount);
+    }
+
+    function mint(address _to, uint256 _amount) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _mint(_to, _amount);
+    }
+
+    function grantAdminRole(address _account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        grantRole(DEFAULT_ADMIN_ROLE, _account);
     }
 }
--- test/WBNB.ts
+++ test/WBNB.ts
@@ -5,27 +5,23 @@ import { parseEther } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
 describe("WBNB", function () {
-
-  let wbnb: Contract
-  let owner: SignerWithAddress
+  let wbnb: Contract;
+  let owner: SignerWithAddress;
 
   this.beforeEach(async () => {
     await ethers.provider.send("hardhat_reset", []);
 
-    [ owner ] = await ethers.getSigners();
+    [owner] = await ethers.getSigners();
 
     const WBNB = await ethers.getContractFactory("WBNB");
-    wbnb = await WBNB.deploy();
+    wbnb = await WBNB.deploy(parseEther("1000000"));
     await wbnb.deployed();
 
-  })
+    await wbnb.mint(owner.address, parseEther("1000"));
+  });
 
   it("should return total supply", async () => {
-    expect(await wbnb.totalSupply()).equal(parseEther('10000'))
-  })
-
-  it('should return valid owner', async () => {
-    expect(await wbnb.owner()).equal(owner.address)
-  })
-
+    expect(await wbnb.totalSupply()).equal(parseEther("1000"));
+    expect(await wbnb.cap()).equal(parseEther("1000000"));
+  });
 });
